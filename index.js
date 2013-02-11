0e978a94ea88ea10be77872dcbead3586587f109
parroit <andrea.parodi@ebansoftware.net>
2013-02-11 13:29:54 +0100

    correct index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-var server=require("server");
+var server=require("./lib/server/server");
 exports.useDb=server.useDb;
 exports.runControllers=server.runControllers;
+
+var auth=exports.auth=require("./lib/server/auth");
+var markdown =  exports.markdown=require("./lib/server/markdown");
+var validator = exports.validator=require("./lib/server/validator");
+auth.init && auth.init(server.app);
+markdown.init && markdown.init(server.app);
+validator.init && validator.init(server.app);
+
+
+exports.mongoId = function (hexId) {
+    var BSON = require('mongodb').BSONPure;
+    return BSON.ObjectID.createFromHexString(hexId);
+};
